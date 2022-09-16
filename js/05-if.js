a140351dc3bf75aaea7cb9f85db8931dc1066652
Mindaugas Krivickas <mkrivickas@gmail.com>
2022-09-16 22:40:51 +0300

    if

---
+++ js/05-if.js
@@ -0,0 +1,20 @@
+/*
+IF
+
+Šablonai:
+if () {}
+
+Palyginimo operatoriai:
+- visi: >, <, <=, >=, ==, !=, ===, !==
+- naudotini: >, <, <=, >=, ===, !==
+- NEnaudotini: ==, !=
+*/
+
+const a = 7;
+const b = '7';
+
+if (a === b) {
+    console.log('A yra lygu B');
+} else {
+    console.log('A nėra lygu B');
+}
