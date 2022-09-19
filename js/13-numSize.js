bf75d64346d6f87d9c3fe27888ec4ea4e6c9537c
Mindaugas Krivickas <mkrivickas@gmail.com>
2022-09-19 16:32:00 +0300

    Number size

--- js/13-numSize.js
+++ js/13-numSize.js
@@ -69,16 +69,43 @@ console.log(Math.max(...[1, 2, 3, 69])); */
 
 // 3 Belovo sprendimas
 function numSize(num) {
-    if (typeof num !== 'number') {
+    if (typeof num !== 'number' || !isFinite(num)) {
         return 'ERROR: netinkamas tipas';
     }
-    return 1;
+    const numAsString = '' + num;
+    let size = numAsString.length;
+
+    // nustatome ar skaičius yra dešimtainis (turi kablelį)
+    if (num % 1 !== 0) {
+        size--;
+    }
+
+    // jei skaičius neigiamas
+    if (num < 0) {
+        size--;
+    }
+
+    return size;
 }
 
-console.log(numSize(5), '-->', 1);
-console.log(numSize(true), '-->', 'ERROR.');
-console.log(numSize('asd'), '-->', 'ERROR.');
-console.log(numSize(NaN), '-->', 'ERROR.');
+// console.log(numSize(true), '-->', 'ERROR.');
+// console.log(numSize('asd'), '-->', 'ERROR.');
+// console.log(numSize(NaN), '-->', 'ERROR.');
+// console.log(numSize(Infinity), '-->', 'ERROR.');
+// console.log(numSize(-Infinity), '-->', 'ERROR.');
+// console.log(numSize([]), '-->', 'ERROR.');
+// console.log(numSize([1]), '-->', 'ERROR.');
+// console.log(numSize(), '-->', 'ERROR.');
+// console.log(numSize(numSize), '-->', 'ERROR.');
 
-console.log(numSize(781), '-->', 3);
-console.log(numSize(37060123456), '-->', 11);
+// console.log(numSize(5), '-->', 1);
+// console.log(numSize(781), '-->', 1);
+// console.log(numSize(37060123456), '-->', 1);
+// console.log(numSize(3.14), '-->', 3);
+// console.log(numSize(2.727), '-->', 4);
+// console.log(numSize(-5), '-->', 1);
+// console.log(numSize(-5.745), '-->', 4);
+
+console.log('EXTRA... (galvos skausmas');
+console.log(numSize(0.000000000001), '-->', 13);
+console.log(numSize(1000000000000000000000), '-->', 22);
